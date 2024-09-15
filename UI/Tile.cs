44c8850b8032f5551c88939f2474f4a8385f9b28
DimaOliinyk <147141430+DimaOliinyk@users.noreply.github.com>
2024-09-15 22:52:34 +0300

    Add files via upload
    Added Classes:
    - Position
    - Tile
    - Grid

---
+++ UI/Tile.cs
@@ -0,0 +1,17 @@
+﻿using CourseWorkUI.Utilities;
+
+namespace CourseWorkUI.UI;
+
+public class Tile
+{
+    public double Width { get; set; }
+    public double Height { get; set; }
+    public Position Position { get; set; }
+
+    public Tile(Position pos, double width, double height)
+    {
+        Position = pos;
+        Width = width;
+        Height = height;
+    }
+}
